319591c636d1680bc177199688cfa5029029e27b
Yegor Feoktistov <yegor_feoktistov1@epam.com>
2018-08-27 15:27:07 +0300

    add client folder to flappy-bird

---
+++ flappy-bird/client/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { TodoEffects, TodosService } from 'store/todo';
+
+import { AppComponent } from './app.component';
+import { reducers } from '../store/store.config';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    MatButtonModule,
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    StoreModule.forRoot(reducers, {}),
+    EffectsModule.forRoot([
+      TodoEffects
+    ]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25
+    }),
+    BrowserAnimationsModule,
+  ],
+  providers: [
+    TodosService,
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
